9c4a559d95dc29d4356659cbceba65ec585e2e96
Jose <jose@joses-mac.local>
2016-09-05 13:17:46 -0700

    Initial Commit

---
+++ Lab6/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Lab6
+//
+//  Created by Jose on 9/5/16.
+//  Copyright © 2016 robert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Lab6/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Lab6
+//
+//  Created by Jose on 9/5/16.
+//  Copyright © 2016 robert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
